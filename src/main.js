b36565bb479ba64a24822ae657eee37c336d20e9
DasomJung24 <jeongdasom6@gmail.com>
2021-04-29 23:23:55 +0900

    main.js

--- src/main.js
+++ src/main.js
@@ -2,8 +2,20 @@ import Vue from "vue";
 import router from "./router";
 import VueRouter from "vue-router";
 import App from "./App.vue";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import {
+  faMusic,
+  faArrowLeft,
+  faArrowRight,
+  faSearch,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 
 Vue.use(VueRouter);
+
+library.add(faMusic, faArrowLeft, faArrowRight, faSearch);
+
+Vue.component("font-awesome-icon", FontAwesomeIcon);
 
 Vue.config.productionTip = false;
 
