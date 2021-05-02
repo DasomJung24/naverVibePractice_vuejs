c9ff35110bb4f842ca493b66de157dec876b650b
DasomJung24 <jeongdasom6@gmail.com>
2021-05-03 00:15:05 +0900

    main.js updqte

--- src/main.js
+++ src/main.js
@@ -3,6 +3,8 @@ import store from "./store";
 import router from "./router";
 import VueRouter from "vue-router";
 import App from "./App.vue";
+import VueAwesomeSwiper from "vue-awesome-swiper";
+import "swiper/css/swiper.css";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import {
   faMusic,
@@ -19,10 +21,15 @@ import {
   faPlay,
   faAngleDoubleLeft,
   faAngleDoubleRight,
+  faSyncAlt,
+  faRandom,
+  faList,
+  faVolumeUp,
 } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 
 Vue.use(VueRouter);
+Vue.use(VueAwesomeSwiper);
 
 library.add(
   faMusic,
@@ -38,7 +45,11 @@ library.add(
   faCalendarCheck,
   faPlay,
   faAngleDoubleRight,
-  faAngleDoubleLeft
+  faAngleDoubleLeft,
+  faSyncAlt,
+  faRandom,
+  faList,
+  faVolumeUp
 );
 
 Vue.component("font-awesome-icon", FontAwesomeIcon);
