5a5dc40bf24dc1bc125bcd4e131b68764455494e
DasomJung24 <jeongdasom6@gmail.com>
2021-05-08 22:37:18 +0900

    search

---
+++ src/components/layouts/Search.vue
@@ -0,0 +1,30 @@
+<template>
+  <div
+    class="search"
+    v-bind:class="{ disableSearch: !$store.getters.getSearchBtn }"
+  >
+    <div class="relative">
+      <font-awesome-icon icon="search" size="2x" class="absoluteSearch" />
+      <input class="input" placeholder="VIBE 검색" />
+      <font-awesome-icon
+        icon="times"
+        size="2x"
+        class="absoluteTimes"
+        @click="closeSearch"
+      />
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "Search",
+  data() {
+    return {};
+  },
+  methods: {
+    closeSearch() {
+      this.$store.commit("changeSearch");
+    },
+  },
+};
+</script>
